b960bc7ca02f650ca0b524426f917ab9852bb266
Juan Jose <alastorfender29@gmail.com>
2025-05-01 12:05:43 +0200

    add validations

--- src/pages/AdvanceForm.jsx
+++ src/pages/AdvanceForm.jsx
@@ -13,11 +13,27 @@ const AdvancedForm = () => {
     <div>
       <h1>Formulario Avanzado</h1>
       <form onSubmit={handleSubmit(onSubmitHandler)}>
-        <label>Nombre:</label>
-        <input type="text" {...register("nombre")} />
-        <br />
         <label>Email:</label>
-        <input type="email" {...register("email")} />
+        <input
+          type="email"
+          {...register("email", {
+            required: true,
+            message: "El email es requerido",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "El email no es valido",
+            },
+          })}
+        />
+        <br />
+        <label>Password:</label>
+        <input
+          type="password"
+          {...register("password", {
+            required: true,
+            minLength: 6,
+          })}
+        />
         <br />
         <button type="submit">Enviar</button>
       </form>
