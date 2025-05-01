ceb1b1cfdcd8148cc7c12baa2c1a6f91595096c4
Juan Jose <alastorfender29@gmail.com>
2025-05-01 12:16:21 +0200

    modified app y main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AdvancedForm from "./pages/AdvanceForm.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AdvancedForm />
+  </StrictMode>
+);
