44602afb7d479dce6c7c6bfe94a2f67f95a02e74
PabloPianelo <pablopianeloxd@gmail.com>
2025-05-08 08:35:11 +0200

    areglar reac-router-dom

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/home";
-import About from "../pages/about";
-import Contact from "../pages/contact";
-import Root from "../routes/Root";
+
+import { Root } from "./root";
 
 const router = createBrowserRouter([
     {
