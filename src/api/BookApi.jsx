99d235a2bd949839483daf8256c853750854a874
PabloPianelo <pablopianeloxd@gmail.com>
2025-06-30 10:35:54 +0200

    .env and VITE_BACKEND_URL

--- src/api/BookApi.jsx
+++ src/api/BookApi.jsx
@@ -1,5 +1,7 @@
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 export const getBooks = async (token) => {
-  const res = await fetch(`http://localhost:3000/books`, {
+  const res = await fetch(`${backendUrl}/books`, {
     method: "GET",
     headers: { "Content-Type": "application/json", authorization: "Bearer " + token },
   });
@@ -8,7 +10,7 @@ export const getBooks = async (token) => {
   }
 
   export const Bookdata = async (token,id) => {
-  const res = await fetch(`http://localhost:3000/books/${id}`, {
+  const res = await fetch(`${backendUrl}/books/${id}`, {
     method: "GET",
     headers: { "Content-Type": "application/json", authorization: "Bearer " + token, },
     
@@ -17,7 +19,7 @@ export const getBooks = async (token) => {
   }
 
    export const rewiusBook = async (data,token,id) => {
-  const res = await fetch(`http://localhost:3000/books/review/${id}`, {
+  const res = await fetch(`${backendUrl}/books/review/${id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json", authorization: "Bearer " + token, },
     body: JSON.stringify(data),
@@ -26,7 +28,7 @@ export const getBooks = async (token) => {
   }
 
  export const deleteRewius = async (data,token,id) => {
-  const res = await fetch(`http://localhost:3000/books/review/${id}?reviewId=${data}`, {
+  const res = await fetch(`${backendUrl}/books/review/${id}?reviewId=${data}`, {
     method: "DELETE",
     headers: { "Content-Type": "application/json", authorization: "Bearer " + token, },
   });
@@ -34,7 +36,7 @@ export const getBooks = async (token) => {
   };
 
 export const getVoteBooks = async (token, bookId, voteType) => {
-  const res = await fetch(`http://localhost:3000/books/${bookId}/vote`, {
+  const res = await fetch(`${backendUrl}/books/${bookId}/vote`, {
     method: "POST",
     headers: { "Content-Type": "application/json", authorization: "Bearer " + token },
 
